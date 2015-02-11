b70f1cc43d24d75d19b3214c1da88515f97d7f85
Jacob Sievers <jacosiev@gmail.com>
2015-02-11 15:45:17 +0100

    Added all views

---
+++ js/view/selectView.js
@@ -0,0 +1,9 @@
+//ExampleView Object constructor
+var SelectView = function (container) {
+  // Get all the relevant elements of the view (ones that show data
+  // and/or ones that responed to interaction)
+  this.numberOfGuests = container.find("#numberOfGuests");
+  this.plusButton = container.find("#plusGuest");
+  this.minusButton = container.find("#minusGuest");
+  this.numberOfGuests.html("Hello World");
+};
