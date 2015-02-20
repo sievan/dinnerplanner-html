043682bca6c4981f1f521dee62cf2de40186daed
Trulls <annaj9@kth.se>
2015-02-20 11:22:58 +0100

    Fixed views

--- js/app.js
+++ js/app.js
@@ -2,26 +2,19 @@ var App = function(model) {
   // Views and model
   var views = {
     selectView: new SelectView($(".select-container"), model),
-    summaryView: new SummaryView($(".summary-container"), model),
     descriptionView: new DescriptionView($(".description-container"), model),
     confirmView: new ConfirmView($('.confirm-container'), model),
     printView: new PrintView($('.print-container'), model),
-    ingredientView: new IngredientView($(".ingredient-container"),model)};
+    summaryView: new SummaryView($(".summary-container"), model),
+    ingredientView: new IngredientView($(".ingredient-container"),model),
+    sumheaderView: new SumheaderView($(".sumheader-container"),model)};
 
   var currentPage = 'index',
       viewsShown = {
-        confirm: function() {
-          this.hideAll();
-          views.confirmView.show();
-        },
         select: function() {
           this.hideAll();
           views.selectView.show();
           views.summaryView.show();
-        },
-        print: function() {
-          this.hideAll();
-          views.printView.show();
         },
         description: function() {
           this.hideAll();
@@ -29,6 +22,17 @@ var App = function(model) {
           views.summaryView.show();
           views.ingredientView.show();
         },
+        confirm: function() {
+          this.hideAll();
+          views.confirmView.show();
+          views.sumheaderView.show();
+        },
+        print: function() {
+          this.hideAll();
+          views.printView.show();
+          views.sumheaderView.show();
+        },
+        
         hideAll: function() {
           _.each(views, function(v, i) {
             v.hide();
--- js/view/descriptionView.js
+++ js/view/descriptionView.js
@@ -4,16 +4,28 @@ var DescriptionView = function (container, model) {
   // Test fixture
   var testDish = model.getDish(1);
   // ----
-  
+
   // Fields
   var dishContainer = container.find('#dish-description');
-  
+
+  // Events
+  $('.confirm-dish').click(function(e) {
+    e.preventDefault();
+    // add dish to menu
+    window.app.switchView('select');
+  });
+  $('.back-button').click(function(e) {
+    e.preventDefault();
+    window.app.switchView('select');
+  });
+
   // Functions
+   
   this.addDishToDescription = function(dish) {
     dishContainer.append('<h1>'+dish.name+'</h1>' +
             '<img src="images/'+dish.image+'"></img>' +
             '<p>Bacon ipsum dolor amet bresaola tongue short loin pig, sausage chuck kielbasa brisket shank filet mignon cupim venison jerky flank. Pork chop shankle shoulder pancetta andouille kielbasa meatball tongue tail ball tip.</p>'+
-            '<a href="selectdish.html" class="btn start-button">Back to Select Dish</a>'+
+            '<a href="selectdish.html" class="btn start-button back-button">Back to Select Dish</a>'+
             '<h2>PREPARATION</h2>'+
             '<p>Bacon ipsum dolor amet bresaola tongue short loin pig, sausage chuck kielbasa brisket shank filet mignon cupim venison jerky flank. Pork chop shankle shoulder pancetta andouille kielbasa meatball tongue tail ball tip.</p>');
   };
