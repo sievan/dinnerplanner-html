767bd772707e1518004110914644dc646f79800d
Jacob Sievers <jacosiev@gmail.com>
2015-02-20 16:47:11 +0100

    Made dishes selectable

--- js/controller/ingredientController.js
+++ js/controller/ingredientController.js
@@ -1,11 +1,13 @@
 var IngredientController = function (view, model) {
   _.bindAll(view, 'update');
+  currentDish = 'hej';
   this.selectDish = function(dish) {
     view.update(model.getDish(dish));
+    currentDish = dish;
   };
   view.confirmButton.click(function(e) {
     e.preventDefault();
-    // TODO add dish to menu
+    model.addDishToMenu(parseInt(currentDish));
     window.app.switchView('select');
   });
 };
